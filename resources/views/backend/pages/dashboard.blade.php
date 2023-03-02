3e12268200b6f5383c1b0a93d63956262d6dafba
Samsun Nahar <samsun.naher.nishi.2017@gmail.com>
2023-03-02 15:49:10 +0600

    dashboard design

--- resources/views/backend/pages/dashboard.blade.php
+++ resources/views/backend/pages/dashboard.blade.php
@@ -1,4 +1,14 @@
 @extends('backend.layouts.layout')
 @section('content')
-<div>Dashboard</div>
+<div class="row m-4">
+    <div class="col-3 dashboard_box m-4">
+       <p class="dashboard_text">Total Company: {{$company}}</p>
+    </div>
+    <div class="col-3 dashboard_box m-4">
+        <p class="dashboard_text">Total Depatment: {{$department}}</p>
+    </div>
+    <div class="col-3 dashboard_box m-4">
+        <p class="dashboard_text">Total Employee: {{$employee}}</p>
+    </div>
+</div>
 @endsection
